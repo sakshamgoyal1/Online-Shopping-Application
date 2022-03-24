130da152c966ec50884792ecf078bbbccb1569d4
sakshamgoyal1 <102207172+sakshamgoyal1@users.noreply.github.com>
2022-03-24 14:54:33 +0530

    Update CustomerController.java

--- shopping-api/src/main/java/in/codevita/onlineshoppingapi/web/CustomerController.java
+++ shopping-api/src/main/java/in/codevita/onlineshoppingapi/web/CustomerController.java
@@ -1,1 +1,27 @@
 
+package in.codevita.onlineshoppingapi.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.codevita.onlineshoppingapi.domain.Customer;
+import in.codevita.onlineshoppingapi.service.CustomerService;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+	
+	@Autowired
+	private CustomerService customerService;
+	
+	@PostMapping("")
+	public Customer createNewCustomer(@RequestBody Customer customer)
+	{
+		Customer savedCustomer= customerService.addCustomer(customer);
+		return savedCustomer;
+	}
+
+}
