7d221a717e3b2378e120c7f18ea4caa6b9ed0729
sakshamgoyal1 <102207172+sakshamgoyal1@users.noreply.github.com>
2022-03-24 12:31:16 +0530

    Create Customer.java

---
+++ shopping-api/src/main/java/in/codevita/onlineshoppingapi/domain/Customer.java
@@ -0,0 +1,60 @@
+package in.codevita.customerapi.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long customerId;
+	private String firstName;
+	private String lastName;
+	private String mobileNumber;
+
+	private String email;
+	public Customer() {
+		super();
+	}
+	public Long getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [customerId=" + customerId + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", mobileNumber=" + mobileNumber + ", email=" + email + "]";
+	}
+
+}
