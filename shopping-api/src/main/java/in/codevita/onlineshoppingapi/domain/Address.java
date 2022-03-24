7912bb00d14e87ef8e6e7ca3b307b8600f660e29
sakshamgoyal1 <102207172+sakshamgoyal1@users.noreply.github.com>
2022-03-24 12:33:52 +0530

    Create Address.java

---
+++ shopping-api/src/main/java/in/codevita/onlineshoppingapi/domain/Address.java
@@ -0,0 +1,70 @@
+package in.codevita.customerapi.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Address {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int addressId;
+	private String streetNo;
+	private String buildingName;
+	private String city;
+	private String state;
+	private String country;
+	private String pincode;
+	public Address() {
+		super();
+	}
+	public int getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+	public String getStreetNo() {
+		return streetNo;
+	}
+	public void setStreetNo(String streetNo) {
+		this.streetNo = streetNo;
+	}
+	public String getBuildingName() {
+		return buildingName;
+	}
+	public void setBuildingName(String buildingName) {
+		this.buildingName = buildingName;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getPincode() {
+		return pincode;
+	}
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+	@Override
+	public String toString() {
+		return "Address [addressId=" + addressId + ", streetNo=" + streetNo + ", buildingName=" + buildingName
+				+ ", city=" + city + ", state=" + state + ", country=" + country + ", pincode=" + pincode + "]";
+	}
+}
