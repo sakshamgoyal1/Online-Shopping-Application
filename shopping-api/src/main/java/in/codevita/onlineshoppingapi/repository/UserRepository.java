b632145dafc3666376ac6aa8e03c1886f4536d4f
Saksham Jauhari <98949528+Sakshamj30@users.noreply.github.com>
2022-03-25 00:03:18 +0530

    Create UserRepository.java

---
+++ shopping-api/src/main/java/in/codevita/onlineshoppingapi/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package in.codevita.onlineshoppingapi.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import in.codevita.onlineshoppingapi.domain.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
