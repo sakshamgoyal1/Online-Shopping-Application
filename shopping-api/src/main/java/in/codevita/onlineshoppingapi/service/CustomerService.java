9d7c2a902f7ffcc73d39d11b4a2e1fd8f8daa9ea
sakshamgoyal1 <102207172+sakshamgoyal1@users.noreply.github.com>
2022-03-26 00:58:51 +0530

    Update CustomerService.java

--- shopping-api/src/main/java/in/codevita/onlineshoppingapi/service/CustomerService.java
+++ shopping-api/src/main/java/in/codevita/onlineshoppingapi/service/CustomerService.java
@@ -1,1 +1,15 @@
+package in.codevita.onlineshoppingapi.service;
 
+import in.codevita.onlineshoppingapi.domain.Customer;
+
+public interface CustomerService {
+
+	public Customer saveorUpdate(Customer customer);
+	
+	public Customer findCustomerByEmailId(String emailId);
+	
+	public Iterable<Customer> finaAllCustomer();
+	
+	public void deleteCustomerByCustomerEmailId(String emailId);
+}
+	
