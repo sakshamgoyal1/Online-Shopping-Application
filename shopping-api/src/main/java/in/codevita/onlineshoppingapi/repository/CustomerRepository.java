b799baa97babfbb8dd244af1f767261c2f240085
sakshamgoyal1 <102207172+sakshamgoyal1@users.noreply.github.com>
2022-03-26 00:58:24 +0530

    Update CustomerRepository.java

--- shopping-api/src/main/java/in/codevita/onlineshoppingapi/repository/CustomerRepository.java
+++ shopping-api/src/main/java/in/codevita/onlineshoppingapi/repository/CustomerRepository.java
@@ -1,1 +1,15 @@
+package in.codevita.onlineshoppingapi.repository;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import in.codevita.onlineshoppingapi.domain.Customer;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long>{
+
+	Customer findByEmail(String email);
+	
+
+	Iterable<Customer> findAll();
+}
