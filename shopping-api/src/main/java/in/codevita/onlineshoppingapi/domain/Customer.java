6da94c8065514797ec4876e6e2ac7892bf561d3d
sakshamgoyal1 <102207172+sakshamgoyal1@users.noreply.github.com>
2022-03-26 17:16:24 +0530

    Update Customer.java

--- shopping-api/src/main/java/in/codevita/onlineshoppingapi/domain/Customer.java
+++ shopping-api/src/main/java/in/codevita/onlineshoppingapi/domain/Customer.java
@@ -1,10 +1,13 @@
 package in.codevita.onlineshoppingapi.domain;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
@@ -24,6 +27,16 @@ public class Customer {
     @NotBlank(message = "Please Enter Your Email Id!!")
     @Column(updatable = false, unique = true)
 	private String email;
+    
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "customer")
+    private Address address;
+    
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 	public Customer() {
 		super();
 	}
@@ -58,11 +71,10 @@ public class Customer {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	@Override
 	public String toString() {
 		return "Customer [customerId=" + customerId + ", firstName=" + firstName + ", lastName=" + lastName
-				+ ", mobileNumber=" + mobileNumber + ", email=" + email + "]";
+				+ ", mobileNumber=" + mobileNumber + ", email=" + email + ", address=" + address + "]";
 	}
 
 }
