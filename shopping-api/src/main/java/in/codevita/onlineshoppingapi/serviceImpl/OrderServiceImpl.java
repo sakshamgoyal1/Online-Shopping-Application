f9211bd32251c04274209a4a2440cbcc142f7427
sakshamgoyal1 <102207172+sakshamgoyal1@users.noreply.github.com>
2022-03-27 21:33:55 +0530

    Update OrderServiceImpl.java

--- shopping-api/src/main/java/in/codevita/onlineshoppingapi/serviceImpl/OrderServiceImpl.java
+++ shopping-api/src/main/java/in/codevita/onlineshoppingapi/serviceImpl/OrderServiceImpl.java
@@ -1,23 +1,39 @@
-package in.codevita.onlineshoppingapi.serviceimpl;
+package in.codevita.onlineshoppingapi.serviceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import in.codevita.onlineshoppingapi.domain.Customer;
 import in.codevita.onlineshoppingapi.domain.Order;
 import in.codevita.onlineshoppingapi.repository.OrderRepository;
 import in.codevita.onlineshoppingapi.service.OrderService;
 
 @Service
 public class OrderServiceImpl implements OrderService{
-	
+
 	@Autowired
-	private OrderRepository orderRepository;
+	public OrderRepository orderrepo;
+	
+	@Override
+	public Order saveorUpdate(Order order) {
+		return orderrepo.save(order);
+	}
 
 	@Override
-	public Order saveOrUpdate(Order order) {
-		//Biz logic
-		return orderRepository.save(order);
+	public Order findOrderByCustomer(Long orderId) {
+		Order order=orderrepo.findByOrderId(orderId);
+		return order;
 	}
-	
+
+	@Override
+	public Iterable<Order> finaAllOrder() {
+		return orderrepo.findAll();
+	}
+
+	@Override
+	public void deleteOrderByCustomer(Long orderId) {
+		Order order=orderrepo.findByOrderId(orderId);
+		orderrepo.delete(order);
+	}
 
 }
