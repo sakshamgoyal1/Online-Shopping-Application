35a2f84b9c39bd84cccf95a5f888abc4b3067f1c
sakshamgoyal1 <102207172+sakshamgoyal1@users.noreply.github.com>
2022-03-27 21:32:57 +0530

    Update OrderService.java

--- shopping-api/src/main/java/in/codevita/onlineshoppingapi/service/OrderService.java
+++ shopping-api/src/main/java/in/codevita/onlineshoppingapi/service/OrderService.java
@@ -1,9 +1,16 @@
 package in.codevita.onlineshoppingapi.service;
 
+import in.codevita.onlineshoppingapi.domain.Customer;
 import in.codevita.onlineshoppingapi.domain.Order;
 
 public interface OrderService {
+
+public Order saveorUpdate(Order order);
 	
-	public Order saveOrUpdate(Order order);
+	public Order findOrderByCustomer(Long orderId);
+	
+	public Iterable<Order> finaAllOrder();
+	
+	public void deleteOrderByCustomer(Long orderId);
 
 }
