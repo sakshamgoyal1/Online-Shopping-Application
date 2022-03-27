fd417b385638bd22422a76aa6486416f45746d51
sakshamgoyal1 <102207172+sakshamgoyal1@users.noreply.github.com>
2022-03-27 14:31:24 +0530

    Update Address.java

--- shopping-api/src/main/java/in/codevita/onlineshoppingapi/domain/Address.java
+++ shopping-api/src/main/java/in/codevita/onlineshoppingapi/domain/Address.java
@@ -1,13 +1,9 @@
 package in.codevita.onlineshoppingapi.domain;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Address {
@@ -15,40 +11,24 @@ public class Address {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long addressId;
-	private String email;
 	private String streetNo;
 	private String buildingName;
 	private String city;
 	private String state;
 	private String country;
 	private String pincode;
-	
-	@OneToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name="customer_id",nullable = false)
-	@JsonIgnore
-	private Customer customer;
-	
+
 	public Address() {
 		super();
 	}
-	public Customer getCustomer() {
-		return customer;
-	}
-	public void setCustomer(Customer customer) {
-		this.customer = customer;
-	}
+
 	public Long getAddressId() {
 		return addressId;
 	}
 	public void setAddressId(Long addressId) {
 		this.addressId = addressId;
 	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
+
 	public String getStreetNo() {
 		return streetNo;
 	}
@@ -84,11 +64,5 @@ public class Address {
 	}
 	public void setPincode(String pincode) {
 		this.pincode = pincode;
-	}
-	@Override
-	public String toString() {
-		return "Address [addressId=" + addressId + ", email=" + email + ", streetNo=" + streetNo + ", buildingName="
-				+ buildingName + ", city=" + city + ", state=" + state + ", country=" + country + ", pincode=" + pincode
-				+ ", customer=" + customer + "]";
 	}
 }
