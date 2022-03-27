c60f8a12e52354b4b1a9fa85821adb95e1e92255
sakshamgoyal1 <102207172+sakshamgoyal1@users.noreply.github.com>
2022-03-27 21:34:39 +0530

    Update OrderController.java

--- shopping-api/src/main/java/in/codevita/onlineshoppingapi/web/OrderController.java
+++ shopping-api/src/main/java/in/codevita/onlineshoppingapi/web/OrderController.java
@@ -1,25 +1,61 @@
 package in.codevita.onlineshoppingapi.web;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import in.codevita.onlineshoppingapi.domain.Customer;
 import in.codevita.onlineshoppingapi.domain.Order;
+import in.codevita.onlineshoppingapi.service.CustomerService;
+import in.codevita.onlineshoppingapi.service.MapValidationErrorService;
 import in.codevita.onlineshoppingapi.service.OrderService;
 
 @RestController
 @RequestMapping("/api/orders")
 public class OrderController {
-	
-	@Autowired
-	private OrderService orderService;
-	@PostMapping("")
-	public Order createNewOrder(@RequestBody Order order) {
-		
-		Order savedOrder=orderService.saveOrUpdate(order);
-		return savedOrder;
-	}
 
-}
+		
+		@Autowired
+		private OrderService orderService;
+		@Autowired
+		private MapValidationErrorService mapValidationErrorService;
+		
+		@PostMapping("")
+		public ResponseEntity<?> createNewOrder(@Valid @RequestBody Order order, BindingResult result)
+		{
+			ResponseEntity<?> errorMap= mapValidationErrorService.mapValidationError(result);
+			if(errorMap!=null) return errorMap;
+			Order savedOrder= orderService.saveorUpdate(order);
+			return new ResponseEntity<Order>(savedOrder,HttpStatus.CREATED);
+		}
+		
+		@GetMapping("/{orderId}")
+		public ResponseEntity<?> getCustomerByEmailId(@PathVariable Long orderId)
+		{
+			Order order=orderService.findOrderByCustomer(orderId);
+			return new ResponseEntity<Order>(order,HttpStatus.OK);
+		}
+		
+		@GetMapping("/all")
+		public Iterable<Order> getAllOrders()
+		{
+			return orderService.finaAllOrder();
+		}
+		
+		@DeleteMapping("/{orderId}")
+		public ResponseEntity<?> deleteOrder(@PathVariable Long orderId)
+		{
+			orderService.deleteOrderByCustomer(orderId);
+			return new ResponseEntity<String>("Customer :"+orderId+" deleted successfully",HttpStatus.OK);
+		}
+	}
