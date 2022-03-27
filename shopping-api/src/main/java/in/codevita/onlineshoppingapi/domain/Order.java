1b85b0632fd71878e868dd83f143fd626a78e9db
sakshamgoyal1 <102207172+sakshamgoyal1@users.noreply.github.com>
2022-03-27 21:31:27 +0530

    Update Order.java

--- shopping-api/src/main/java/in/codevita/onlineshoppingapi/domain/Order.java
+++ shopping-api/src/main/java/in/codevita/onlineshoppingapi/domain/Order.java
@@ -1,103 +1,96 @@
 package in.codevita.onlineshoppingapi.domain;
 
 import java.util.Date;
-import java.util.Map;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
+@Table(name = "orders")
 public class Order {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long orderId;
-	private Date orderDate;
-	private String orderStatus;
-	private Map<Long,String> productlist;
-	
-	
-	private Date created_At;
-	private Date updated_At;
-	
+	public Long orderId;
+	public Date orderDate;
+	public String orderStatus;
 	
 	public Order() {
 		super();
 	}
 	
-	
+	@OneToOne(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+	@JoinColumn(referencedColumnName = "customerId")
+	public Customer customer;
+	private Date created_At;
+	private Date updated_At;
 	public Long getOrderId() {
 		return orderId;
 	}
-
 
 	public void setOrderId(Long orderId) {
 		this.orderId = orderId;
 	}
 
-
 	public Date getOrderDate() {
 		return orderDate;
 	}
-
 
 	public void setOrderDate(Date orderDate) {
 		this.orderDate = orderDate;
 	}
 
-
 	public String getOrderStatus() {
 		return orderStatus;
 	}
 
-
 	public void setOrderStatus(String orderStatus) {
 		this.orderStatus = orderStatus;
+	}	
+	public Customer getCustomer() {
+		return customer;
 	}
 
-	public Map<Long, String> getProductlist() {
-		return productlist;
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	@PrePersist
+	public void onCreate() {
+		this.setCreated_At(new Date());
 	}
 
-
-	public void setProductlist(Map<Long, String> productlist) {
-		this.productlist = productlist;
+	@PreUpdate
+	public void onUpdate() {
+		this.setUpdated_At(new Date());
 	}
-
 
 	public Date getCreated_At() {
 		return created_At;
 	}
 
-
 	public void setCreated_At(Date created_At) {
 		this.created_At = created_At;
 	}
-
 
 	public Date getUpdated_At() {
 		return updated_At;
 	}
 
-
 	public void setUpdated_At(Date updated_At) {
 		this.updated_At = updated_At;
 	}
-
-
-	@PrePersist
-	public void onCreate() {
-		this.created_At= new Date();
-		
-	}
-	
-	@PreUpdate
-	public void onUpdate() {
-		this.updated_At= new Date();
-	}
-
 }
