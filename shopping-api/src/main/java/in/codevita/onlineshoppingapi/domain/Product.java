56f6bd0f67aeeeade9150ebaa93c2070f96ea83c
sakshamgoyal1 <102207172+sakshamgoyal1@users.noreply.github.com>
2022-03-28 22:04:13 +0530

    Create Product.java

---
+++ shopping-api/src/main/java/in/codevita/onlineshoppingapi/domain/Product.java
@@ -0,0 +1,75 @@
+package in.codevita.onlineshoppingapi.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long productId;
+	
+	private String productName;
+	private String color;
+	private String price;
+	private String dimension;
+	private String specification;
+	private String manufacturer;
+	private String quantity;
+	
+	public Product() {
+		super();
+	}
+	public Long getProductId() {
+		return productId;
+	}
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getDimension() {
+		return dimension;
+	}
+	public void setDimension(String dimension) {
+		this.dimension = dimension;
+	}
+	public String getSpecification() {
+		return specification;
+	}
+	public void setSpecification(String specification) {
+		this.specification = specification;
+	}
+	public String getManufacturer() {
+		return manufacturer;
+	}
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+	public String getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+
+}
