577bd8ecefc91d9cb17758f2c7f1c1b9e688665e
sakshamgoyal1 <102207172+sakshamgoyal1@users.noreply.github.com>
2022-03-28 11:37:58 +0530

    Update AddressRepository.java

--- shopping-api/src/main/java/in/codevita/onlineshoppingapi/repository/AddressRepository.java
+++ shopping-api/src/main/java/in/codevita/onlineshoppingapi/repository/AddressRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import in.codevita.onlineshoppingapi.domain.Address;
+import in.codevita.onlineshoppingapi.domain.Customer;
 
 @Repository
 public interface AddressRepository extends CrudRepository<Address, Long>{
-
-
+Address findByAddressId(Long addressId);
 	
+
+	Iterable<Address> findAll();
+
 }
