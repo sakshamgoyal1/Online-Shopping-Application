6fe38bd32cd95b971b3a3cf4db727c41542262ca
Saksham Jauhari <98949528+Sakshamj30@users.noreply.github.com>
2022-03-29 04:29:43 +0530

    Update UserService.java

--- shopping-api/src/main/java/in/codevita/onlineshoppingapi/service/UserService.java
+++ shopping-api/src/main/java/in/codevita/onlineshoppingapi/service/UserService.java
@@ -5,6 +5,13 @@ import in.codevita.onlineshoppingapi.domain.User;
 public interface UserService {
 
 	public User saveOrUpdate(User user);
+	
+	public User findUserByEmail(String projectId);
+	
+	public Iterable<User> FindAllUser();
+	
+	public void deleteUserByEmail(String projectId);
+	
 
 	
 }
