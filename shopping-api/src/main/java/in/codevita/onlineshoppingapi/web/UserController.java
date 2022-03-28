fc10137ce60630ad79ae891e6b1daba75cb31cf6
Saksham Jauhari <98949528+Sakshamj30@users.noreply.github.com>
2022-03-29 04:33:43 +0530

    Update UserController.java

--- shopping-api/src/main/java/in/codevita/onlineshoppingapi/web/UserController.java
+++ shopping-api/src/main/java/in/codevita/onlineshoppingapi/web/UserController.java
@@ -1,12 +1,21 @@
 package in.codevita.onlineshoppingapi.web;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.codevita.onlineshoppingapi.domain.User;
+import in.codevita.onlineshoppingapi.service.MapValidationErrorService;
 import in.codevita.onlineshoppingapi.service.UserService;
 @RestController
 @RequestMapping("/api/users")
@@ -15,13 +24,35 @@ public class UserController{
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private MapValidationErrorService mapValidationErrorService;
+	
 	@PostMapping("")
-	public User createNewUser(@RequestBody User user) {
+	public ResponseEntity<?> createNewUser(@Valid @RequestBody User user, BindingResult result) {
+		ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationError(result);
+		if(errorMap!=null) return errorMap;
 		User savedUser = userService.saveOrUpdate(user);
-		return savedUser;
-		
+		return new ResponseEntity<User>(savedUser,HttpStatus.CREATED);
 	}
 	
+		@GetMapping("/{projectId}")
+		public ResponseEntity<?> getUserById(@PathVariable String projectId) {
+		User user = userService.findUserByEmail(projectId);
+		return new ResponseEntity<User>(user,HttpStatus.OK);
 	
-	
+		}
+		
+		@GetMapping("/all")
+		public Iterable<User> getAllUsers(){
+			return userService.FindAllUser();
+		}
+		@DeleteMapping("/{projectId}")
+		public ResponseEntity<?> deleteUser(@PathVariable String projectId){
+			
+			userService.deleteUserByEmail(projectId);
+			return new ResponseEntity<String>("User with Id:" +projectId.toUpperCase()+" deleted successfully!!",HttpStatus.OK);
+			
+			
+			
+		}
 }
