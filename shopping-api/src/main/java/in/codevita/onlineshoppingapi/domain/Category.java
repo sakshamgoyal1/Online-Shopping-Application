03cdcd4c3a33f9bf6e78e2599481d4c17bde7589
sakshamgoyal1 <102207172+sakshamgoyal1@users.noreply.github.com>
2022-03-29 12:12:28 +0530

    Create Category.java

---
+++ shopping-api/src/main/java/in/codevita/onlineshoppingapi/domain/Category.java
@@ -0,0 +1,30 @@
+package in.codevita.onlineshoppingapi.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long catId;
+	public String categoryName;
+	public Category() {
+		super();
+	}
+	public Long getCatId() {
+		return catId;
+	}
+	public void setCatId(Long catId) {
+		this.catId = catId;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+}
