d50d5aa181f52c59ddeb72950b03a8f5d428a34d
sakshamgoyal1 <102207172+sakshamgoyal1@users.noreply.github.com>
2022-03-30 13:08:23 +0530

    Update User.java

--- shopping-api/src/main/java/in/codevita/onlineshoppingapi/domain/User.java
+++ shopping-api/src/main/java/in/codevita/onlineshoppingapi/domain/User.java
@@ -1,100 +1,102 @@
 package in.codevita.onlineshoppingapi.domain;
 
 import java.util.Date;
+import java.util.List;
 
-import javax.persistence.Column;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+
+import org.aspectj.weaver.ast.Or;
+import org.hibernate.type.TrueFalseType;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
+
 public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long number;
-	@NotBlank(message = "firstName is required")
-	private String firstName;
-	@NotBlank(message = "email is required")
-	@Column(updatable = false, unique = true)
-	private String email;
-	@NotBlank(message = "password is required")
-	@Size(min = 4, max = 12, message = "create password between 4 to 12 characters limit.")
+	private Long userId;
+	@NotBlank(message = "Please enter your password")
 	private String password;
-	private Date start_date;
-	private Date end_date;
-	
+//	@JoinColumn(unique = true, updatable = false)
+//	private String mobileNumber;
+	@JsonFormat(pattern="yyy-MM-dd")
 	private Date created_At;
+	@JsonFormat(pattern="yyy-MM-dd")
 	private Date updated_At;
 	
+	@OneToOne(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+	private Customer customer;
 	
-	public User() {
-		super();
+	public Long getUserId() {
+		return userId;
 	}
-	public Long getNumber() {
-		return number;
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
 	}
-	public void setNumber(Long number) {
-		this.number = number;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getemail() {
-		return email;
-	}
-	public void setemail(String secondName) {
-		this.email = secondName;
-	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public Date getStart_date() {
-		return start_date;
-	}
-	public void setStart_date(Date start_date) {
-		this.start_date = start_date;
-	}
-	public Date getEnd_date() {
-		return end_date;
-	}
-	public void setEnd_date(Date end_date) {
-		this.end_date = end_date;
-	}
+
 	public Date getCreated_At() {
 		return created_At;
 	}
+
 	public void setCreated_At(Date created_At) {
 		this.created_At = created_At;
 	}
+
 	public Date getUpdated_At() {
 		return updated_At;
 	}
+
 	public void setUpdated_At(Date updated_At) {
 		this.updated_At = updated_At;
 	}
-	
-	
-	
+
 	@PrePersist
 	public void onCreate() {
-		this.created_At=new Date();
+		this.created_At = new Date();
 	}
-	
+
 	@PreUpdate
 	public void onUpdate() {
-		this.updated_At = new Date();	
+		this.updated_At = new Date();
 	}
-	
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+//	public String getMobileNumber() {
+//		return mobileNumber;
+//	}
+//
+//	public void setMobileNumber(String mobileNumber) {
+//		this.mobileNumber = mobileNumber;
+//	}
+
 }
