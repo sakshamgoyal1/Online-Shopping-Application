30ff44fdcd8c9c03334a5de653a114e0cfd8f8d6
sakshamgoyal1 <102207172+sakshamgoyal1@users.noreply.github.com>
2022-04-25 22:02:15 +0530

    Update index.js

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,1 +1,8 @@
 
+import { combineReducers } from "redux";
+import CustomerReducer from "./CustomerReducer";
+import errorReducer from "./errorReducer";
+export default combineReducers({
+    errors:errorReducer,
+    customers: CustomerReducer,
+})
