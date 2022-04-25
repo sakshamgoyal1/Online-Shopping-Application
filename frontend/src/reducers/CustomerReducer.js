6f7e00839b47a0896c073becb526116028ac117c
sakshamgoyal1 <102207172+sakshamgoyal1@users.noreply.github.com>
2022-04-25 22:01:33 +0530

    Update CustomerReducer.js

--- frontend/src/reducers/CustomerReducer.js
+++ frontend/src/reducers/CustomerReducer.js
@@ -1,1 +1,29 @@
 
+import { GET_CUSTOMERS, GET_CUSTOMER, DELETE_CUSTOMER } from "../actions/types";
+const initialState={
+    customers:[],
+    customer:{}
+}
+
+export default function(state=initialState,action){
+    switch(action.type){
+        case GET_CUSTOMERS:
+            return{
+                ...state,
+                customers:action.payload,
+            }
+            case GET_CUSTOMER:
+                return{
+                    ...state,
+                    customer: action.payload,
+                }
+
+            case DELETE_CUSTOMER:
+                return{
+                    ...state,
+                    customers:action.payload,
+                }
+            default:
+                return state
+    }
+}
